4965d19f9928871427924d5d6e2c9e5030a1b1cc
Marco Santarelle da Silva Jardim <marco.j@samsung.com>
2017-12-11 15:16:17 -0400

    projects spring boot of course Master Hibernate and JPA with Spring Boot in 100 steps from Udemy

---
+++ database-demo/src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -0,0 +1,34 @@
+package com.in28minutes.database.databasedemo;
+
+import com.in28minutes.database.databasedemo.jdbc.PersonJdbcDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class DatabaseDemoApplication implements CommandLineRunner {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    PersonJdbcDao dao;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseDemoApplication.class, args);
+	}
+
+
+    @Override
+    public void run(String... strings) throws Exception {
+        logger.info("All users -> {}", dao.findAll());
+        logger.info("User id 10001 -> {}", dao.findById(10001));
+        logger.info("Deleting 10002 -> No of Rows Deleted - {}", dao.deleteById(10002));
+        logger.info("Inserting 10004 -> {}", dao.insert(new Person(10004, "Tara", "Berlin", new Date())));
+        logger.info("Updating 10003 -> {}", dao.update(new Person(10003, "Pieter", "Utrecht", new Date())));
+    }
+}
---
+++ springbootin10steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/Springbootin10stepsApplication.java
@@ -0,0 +1,12 @@
+package com.in28minutes.springboot.basics.springbootin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootin10stepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootin10stepsApplication.class, args);
+	}
+}
